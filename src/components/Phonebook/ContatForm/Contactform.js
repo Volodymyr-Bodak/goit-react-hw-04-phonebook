272a7d7c9150a0d7dcd40a8766070e722e980f35
Volodymyr-Bodak <122619914+Volodymyr-Bodak@users.noreply.github.com>
2023-08-05 17:39:08 +0200

    Update Contactform.js

--- src/components/Phonebook/ContatForm/Contactform.js
+++ src/components/Phonebook/ContatForm/Contactform.js
@@ -1,58 +1,59 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
-import styles from 'components/Phonebook/ContatForm/ContactForm.module.css'
-export default class ContactForm extends Component {
- 
+import styles from 'components/Phonebook/ContatForm/ContactForm.module.css';
 
-  state = {
-    name: "",
-    number: "",
-  };
+const ContactForm = ({ handleSubmit }) => {
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
 
-  handleChange = (event) => {
+  const handleChange = (event) => {
     const { name, value } = event.target;
-    this.setState({ [name]: value });
+    if (name === "name") {
+      setName(value);
+    } else if (name === "number") {
+      setNumber(value);
+    }
   };
 
-  handleSubmit = (event) => {
+  const onSubmit = (event) => {
     event.preventDefault();
-    const { name, number } = this.state;
     const newContact = { id: Date.now().toString(), name, number };
-    this.props.handleSubmit(newContact);
-    this.setState({ name: "", number: "" });
+    handleSubmit(newContact);
+    setName("");
+    setNumber("");
   };
 
-  render() {
-    const { name, number } = this.state;
-    return (
-      <form className={styles.formContainer} onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input
-            className={styles.formInput}
-            type="text"
-            name="name"
-            value={name}
-            onChange={this.handleChange}
-          />
-        </label>
-        <label>
-          Number:
-          <input
-            className={styles.formInput}
-            type="text"
-            name="number"
-            value={number}
-            onChange={this.handleChange}
-          />
-        </label>
-        <button className={styles.formButton} type="submit">
-          Add Contact
-        </button>
-      </form>
-    );
-  }
-}
+  return (
+    <form className={styles.formContainer} onSubmit={onSubmit}>
+      <label>
+        Name:
+        <input
+          className={styles.formInput}
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Number:
+        <input
+          className={styles.formInput}
+          type="text"
+          name="number"
+          value={number}
+          onChange={handleChange}
+        />
+      </label>
+      <button className={styles.formButton} type="submit">
+        Add Contact
+      </button>
+    </form>
+  );
+};
+
 ContactForm.propTypes = {
   handleSubmit: PropTypes.func.isRequired,
 };
+
+export default ContactForm;
