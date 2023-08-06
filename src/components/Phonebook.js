fa150c3bfe771021461c94495e4034442610a171
Volodymyr-Bodak <122619914+Volodymyr-Bodak@users.noreply.github.com>
2023-08-06 20:06:09 +0200

    Update Phonebook.js

--- src/components/Phonebook.js
+++ src/components/Phonebook.js
@@ -1,23 +1,15 @@
-import React, { useState, useEffect } from "react";
-import ContactForm from "./Phonebook/ContatForm/Contactform";
-import ContactList from "./Contactlist";
+import React, { useState } from "react";
+import ContactForm from "./Phonebook/ContactForm/ContactForm";
+import ContactList from "./ContactList";
 import Filter from "./Filter";
 import PropTypes from "prop-types";
 
 const Phonebook = () => {
-  const [contacts, setContacts] = useState(null);
+  const [contacts, setContacts] = useState(() => {
+    const storedContacts = localStorage.getItem("contacts");
+    return storedContacts ? JSON.parse(storedContacts) : [];
+  });
   const [filter, setFilter] = useState("");
-
-  useEffect(() => {
-    const fetchContacts = async () => {
-      const storedContacts = localStorage.getItem("contacts");
-      if (storedContacts) {
-        setContacts(JSON.parse(storedContacts));
-      }
-    };
-
-    fetchContacts();
-  }, []);
 
   const handleChangeFilter = (event) => {
     setFilter(event.target.value);
@@ -33,20 +25,16 @@ const Phonebook = () => {
       return;
     }
 
-    setContacts((prevContacts) => (prevContacts ? [...prevContacts, newContact] : [newContact]));
+    setContacts((prevContacts) => [...prevContacts, newContact]);
   };
 
   const handleDelete = (id) => {
-    setContacts((prevContacts) => (prevContacts ? prevContacts.filter((contact) => contact.id !== id) : null));
+    setContacts((prevContacts) => prevContacts.filter((contact) => contact.id !== id));
   };
 
   const filteredContacts = contacts?.filter((contact) =>
     contact.name.toLowerCase().includes(filter.toLowerCase())
   );
-
-  if (contacts === null) {
-    return <div>Loading...</div>;
-  }
 
   return (
     <div>
@@ -58,6 +46,7 @@ const Phonebook = () => {
     </div>
   );
 };
+
 Phonebook.propTypes = {
   contacts: PropTypes.arrayOf(
     PropTypes.shape({
