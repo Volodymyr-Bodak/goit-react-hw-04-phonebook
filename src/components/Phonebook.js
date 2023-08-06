da8c3652f07cd4586ae0e5c203f9e91d656b8644
Volodymyr-Bodak <122619914+Volodymyr-Bodak@users.noreply.github.com>
2023-08-06 21:47:11 +0200

    Update Phonebook.js

--- src/components/Phonebook.js
+++ src/components/Phonebook.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import ContactForm from "./Phonebook/ContactForm/ContactForm";
-import ContactList from "./ContactList";
+import ContactForm from "./Phonebook/ContatForm/Contactform";
+import ContactList from "./Contactlist";
 import Filter from "./Filter";
 import PropTypes from "prop-types";
 
